ad19724edce1abb77b41a57acf84b0c57675549d
Patrick Moore <moorepa@outlook.com>
2018-03-31 12:19:06 +0100

    improved count() function to allow it to filter based on the iris type

--- irisanalyser.py
+++ irisanalyser.py
@@ -6,11 +6,16 @@
 
 # Section 1 - Functions - In this section of the project, the functions used to analyse the data are defined
 
-def count():                                     # count() is a function that will count the number of rows in the data file
-  with open("data/iris.csv", "r") as myfile:     # this statement opens the iris data set as an object called myfile
+def count(x,type):                                        # count() is a function that will count the number of rows in the data file
+  with open("data/iris.csv", "r") as myfile:        # this statement opens the iris data set as an object called myfile
     counter = 0                                  # this initialises a variable that will used to store the values of the count
     for line in myfile:                          # this loop run through the rows of the file one at a time
-      counter = counter + 1                      # for each pass through the loop the counter is incremented by one
+      rows = line.split(',')[0:5]
+      if type == 'all':
+        counter = counter + 1                      # for each pass through the loop the counter is incremented by one
+      else:
+        if type in rows[4]:
+          counter = counter + 1    
     return counter                               # the counter is returned at the end with the number of rows in the data set
 
 def sum(x):                                      # sum(x) is a function that takes an argument of 'x' and then returns the sum of the values in column 'x'
@@ -21,52 +26,53 @@ def sum(x):                                      # sum(x) is a function that tak
       sums = sums + float(rows[x])               # on each pass through the loop sums is incremented with the float value of the line for the column specified by 'x'
     return sums                                  # the final value of the sum is returned with the summation of all values in the specified column
 
-def mean(x):                                     # mean(x) takes a value x and returns the mean of the column x 
-  average = sum(x)/count()                       # the functions that are already declared can be used to calculate the average 
-  return average
+#def mean(x):                                     # mean(x) takes a value x and returns the mean of the column x 
+ # average = sum(x)/count(x,type)                       # the functions that are already declared can be used to calculate the average 
+  #return average
 
-def max(x):                                       # max(x) is a function that takes an argument of 'x' and then returns the maximum value in column 'x'
-  with open("data/iris.csv", "r") as myfile:      # this statement opens the iris data set as an object called myfile                   
-    maximum = 0                                   # this initialises a variable that will used to store the maximum value in column 'x'
-    for line in myfile:                           # this loop run through the rows of the file one at a time
-      rows = line.split(',')[0:5]                 # rows is a new string that temporarily stores the data values from each line split into seperate entries per column of the data set
-      if float(rows[x]) > maximum:                # on each pass through the loop the value is checked to see if it is greater than 'maximum'
-        maximum = float(rows[x])                  # if it is greater than 'maximum' then 'maximum' will be set to that value
-    return maximum                                # the final value of 'maximum' is returned
+#def max(x):                                       # max(x) is a function that takes an argument of 'x' and then returns the maximum value in column 'x'
+ # with open("data/iris.csv", "r") as myfile:      # this statement opens the iris data set as an object called myfile                   
+  #  maximum = 0                                   # this initialises a variable that will used to store the maximum value in column 'x'
+   # for line in myfile:                           # this loop run through the rows of the file one at a time
+    #  rows = line.split(',')[0:5]                 # rows is a new string that temporarily stores the data values from each line split into seperate entries per column of the data set
+     # if float(rows[x]) > maximum:                # on each pass through the loop the value is checked to see if it is greater than 'maximum'
+      #  maximum = float(rows[x])                  # if it is greater than 'maximum' then 'maximum' will be set to that value
+   # return maximum                                # the final value of 'maximum' is returned
 
-def min(x):                                       # min(x) is a function that takes an argument of 'x' and then returns the minimum value in column 'x'
-  with open("data/iris.csv", "r") as myfile:      # this statement opens the iris data set as an object called myfile                           
-    minimum = 10                                  # this initialises a variable that will used to store the maximum value in column 'x' (value can't be 0 to start otherwise it will not change in the for loop)
-    for line in myfile:                           # this loop run through the rows of the file one at a time
-      rows = line.split(',')[0:5]                 # rows is a new string that temporarily stores the data values from each line split into seperate entries per column of the data set
-      if float(rows[x]) < minimum:                # on each pass through the loop the value is checked to see if it is less than 'minimum'
-        minimum = float(rows[x])                  # if it is less than 'minimum' then 'minimum' will be set to that value
-    return minimum                                # the final value of 'minimum' is returned
+#def min(x):                                       # min(x) is a function that takes an argument of 'x' and then returns the minimum value in column 'x'
+ # with open("data/iris.csv", "r") as myfile:      # this statement opens the iris data set as an object called myfile                           
+  #  minimum = 10                                  # this initialises a variable that will used to store the maximum value in column 'x' (value can't be 0 to start otherwise it will not change in the for loop)
+   # for line in myfile:                           # this loop run through the rows of the file one at a time
+    #  rows = line.split(',')[0:5]                 # rows is a new string that temporarily stores the data values from each line split into seperate entries per column of the data set
+     # if float(rows[x]) < minimum:                # on each pass through the loop the value is checked to see if it is less than 'minimum'
+      #  minimum = float(rows[x])                  # if it is less than 'minimum' then 'minimum' will be set to that value
+    #return minimum                                # the final value of 'minimum' is returned
 
-def stddev(x):                                    # stddev(x) is a function that takes an argument of 'x' and then returns the standard deviation of the data in column 'x'
-  with open("data/iris.csv", "r") as myfile:      # this statement opens the iris data set as an object called myfile
-    devsq = 0                                     # this initialises a variable that will used to store the "deviations from the mean"
-    for line in myfile:                           # this loop run through the rows of the file one at a time
-      rows = line.split(',')[0:5]                 # rows is a new string that temporarily stores the data values from each line split into seperate entries per column of the data set
-      devsq = devsq + (mean(x) - float(rows[x]))**2 # on each pass through the loop devsq is incremented by the value of the 'deviation from the mean' squared
-    sig = (devsq/count())**0.5                    # sig is the standard deviation calulated by getting the square root of the average of 'devsq'
-    return sig                                    # this calculation for sig is returned
+#def stddev(x):                                    # stddev(x) is a function that takes an argument of 'x' and then returns the standard deviation of the data in column 'x'
+ # with open("data/iris.csv", "r") as myfile:      # this statement opens the iris data set as an object called myfile
+  #  devsq = 0                                     # this initialises a variable that will used to store the "deviations from the mean"
+   # for line in myfile:                           # this loop run through the rows of the file one at a time
+    #  rows = line.split(',')[0:5]                 # rows is a new string that temporarily stores the data values from each line split into seperate entries per column of the data set
+     # devsq = devsq + (mean(x) - float(rows[x]))**2 # on each pass through the loop devsq is incremented by the value of the 'deviation from the mean' squared
+    #sig = (devsq/count(x,type))**0.5                    # sig is the standard deviation calulated by getting the square root of the average of 'devsq'
+    #return sig                                    # this calculation for sig is returned
 
 # Section 2 - Data Summary - In this section of the project, the functions are used to summarise the data, all the data is formatted to 2 decimla places and converted to strings to improve printing
-summary = [[' ', 'Sepal Lenght (cm)', 'Sepal Width (cm)', 'Petal Lenght (cm)','Petal Width'],
-           ['Sum', str(round(sum(0),2)), str(round(sum(1),2)), str(round(sum(2),2)), str(round(sum(3),2))],
-           ['Max', str(round(max(0),2)), str(round(max(1),2)), str(round(max(2),2)), str(round(max(3),2))],
-           ['Min', str(round(min(0),2)), str(round(min(1),2)), str(round(min(2),2)), str(round(min(3),2))],
-           ['Mean', str(round(mean(0),2)), str(round(mean(1),2)), str(round(mean(2),2)), str(round(mean(3),2))],
-           ['Std Dev', str(round(stddev(0),2)), str(round(stddev(1),2)), str(round(stddev(2),2)), str(round(stddev(3),2))]
-          ]
+#summary = [[' ', 'Sepal Lenght (cm)', 'Sepal Width (cm)', 'Petal Lenght (cm)','Petal Width (cm)'],
+ #          ['Sum', str(round(sum(0),2)), str(round(sum(1),2)), str(round(sum(2),2)), str(round(sum(3),2))],
+  #         ['Max', str(round(max(0),2)), str(round(max(1),2)), str(round(max(2),2)), str(round(max(3),2))],
+   #        ['Min', str(round(min(0),2)), str(round(min(1),2)), str(round(min(2),2)), str(round(min(3),2))],
+    #       ['Mean', str(round(mean(0),2)), str(round(mean(1),2)), str(round(mean(2),2)), str(round(mean(3),2))],
+     #      ['Std Dev', str(round(stddev(0),2)), str(round(stddev(1),2)), str(round(stddev(2),2)), str(round(stddev(3),2))]
+      #    ]
                                                                                                                                                                                                                 
-def output(data):                                  # I researched how to format the out put here https://stackoverflow.com/questions/9989334/create-nice-column-output-in-python 
-  colwidth = 18                                    # 'output' takes a list as an argument 
-  for row in data:                                 # it loops through each row in the list   
-    print("".join(cell.ljust(colwidth) for cell in row)) # it prints each item in the row formatted to the column width specified
+#def output(data):                                  # I researched how to format the out put here https://stackoverflow.com/questions/9989334/create-nice-column-output-in-python 
+ # colwidth = 18                                    # 'output' takes a list as an argument 
+  #print('The data summary for all is: ')
+  #for row in data:                                 # it loops through each row in the list   
+   # print("".join(cell.ljust(colwidth) for cell in row)) # it prints each item in the row formatted to the column width specified
 
-output(summary)
+#output(summary)
     
 #import numpy as np
 #print(np.matrix(summary))
@@ -79,3 +85,8 @@ output(summary)
 #print(round(stddev(2),2))
 #print(round(stddev(3),2))
 #print(summary)
+
+print(count(0,'all'))
+print(count(0,'setosa'))
+print(count(0,'versicolor'))
+print(count(0,'virginica'))
